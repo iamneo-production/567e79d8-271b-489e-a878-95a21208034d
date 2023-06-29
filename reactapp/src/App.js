1590a6d94cac098c8d2cc2177bf6471c0696dce2
arvin2754 <aravindh2709@gmail.com>
2023-06-29 10:29:21 +0530

    Settings and changepasword

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Website from "./Pages/Website";
 import Login from "./Pages/Auth/Login";
 import Register from "./Pages/Auth/Register";
+import Profile from "./Pages/Settings/Profilesettings";
+import Changepass from "./Pages/Auth/Changepass/Changepassword";
 
 function App() {
   return (
@@ -12,6 +14,9 @@ function App() {
           <Route path="/" element={<Website />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/Settings" element={<Profile />} />
+          <Route path="/pwd" element={<Changepass />} />
+
         </Routes>
       </BrowserRouter>
     </div>
