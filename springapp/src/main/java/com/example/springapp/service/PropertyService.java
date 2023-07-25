b50940cfde9aececf669ec2084d3330ca7deff34
RishikC25 <132253336+RishikC25@users.noreply.github.com>
2023-07-26 01:39:14 +0530

    Update PropertyService.java

--- springapp/src/main/java/com/example/springapp/service/PropertyService.java
+++ springapp/src/main/java/com/example/springapp/service/PropertyService.java
@@ -23,6 +23,26 @@ public class PropertyService {
 		propertyRepo.findAll().forEach(properties::add);
 		return properties;
 	}
+	public int findpending(){
+		int count=0;
+		List<Property>properties=new ArrayList<>();
+		propertyRepo.findAll().forEach(properties::add);
+		for(int i=0;i<properties.size();i++){
+			if(properties.get(i).getStatus().equals("Pending"))
+				count++;
+		}
+		return count;
+	}
+	public int findcancelled(){
+		int count=0;
+		List<Property>properties=new ArrayList<>();
+		propertyRepo.findAll().forEach(properties::add);
+		for(int i=0;i<properties.size();i++){
+			if(properties.get(i).getStatus().equals("Cancelled"))
+				count=count+1;
+		}
+		return count;
+	}
 
 	public void addProperty(Property property) {
 		propertyRepo.save(property);
